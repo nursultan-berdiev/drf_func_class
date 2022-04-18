81961edfedab02698180c561db8a7bf4c8d2bcb0
Nursultan Berdiev <nursultanberdiev1994@gmail.com>
2022-04-18 18:30:39 +0600

    for lesson

--- ser_lesson/urls.py
+++ ser_lesson/urls.py
@@ -4,8 +4,4 @@ from book import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.genre_view),
-    path('book/', views.BookView.as_view()),
-    path('<int:pk>/', views.genre_detail),
-    path('book/<int:pk>/', views.BookDetailView.as_view()),
 ]
